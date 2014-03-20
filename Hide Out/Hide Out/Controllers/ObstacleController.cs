87ca70424d80a704e1b0891d6dbae2642016696c
rjb3vp <rjb3vp@virginia.edu>
2014-03-20 18:25:27 -0400

    Update ObstacleController.cs
    Ryan's update

--- Hide Out/Hide Out/Controllers/ObstacleController.cs
+++ Hide Out/Hide Out/Controllers/ObstacleController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +7,29 @@ namespace Hide_Out.Controllers
 {
     class ObstacleController
     {
+        double clicksSinceLastRandomize = 0;
+        double refreshInterval = 10000;
+        public ObstacleController()
+        {
+
+        }
+
+        public void initialize() {
+              clicksSinceLastRandomize = 0;
+        }
+
+        public void update(double elapsedMilliseconds)
+        {
+            clicksSinceLastRandomize += elapsedMilliseconds;
+
+            if (clicksSinceLastRandomize > refreshInterval)
+            {
+                clicksSinceLastRandomize = 0;
+                //loop through all trash cans, empty them
+                //add something in a random trash can
+            }
+
+        }
+
     }
 }
